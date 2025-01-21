e0fd37c03463c62e0ba01482cc6880e07ff991a9
Luis-GR05 <lgordillor01@iesalbarregas.es>
2025-01-21 17:35:04 +0100

    Update Usuario.java

--- Biblioteca/src/Usuario.java
+++ Biblioteca/src/Usuario.java
@@ -1,2 +1,14 @@
-public class Usuario{
+public class Usuario {
+    public class usuario {
+        String nombre;
+        String IDusuario;
+        String rol;
+        boolean prestadoActivo;
+
+        private String rolDefecto = "usuario";
+        private String prestadoDefecto = "false";
+
+
+    }
+
 }
